a54af421c2a42983fccb469f4206734d0fa89378
Jose Figueroa <joselfigueroag@gmail.com>
2021-04-22 19:54:33 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace 'api' do
+    namespace 'v1' do
+      resources :movie_series do
+        resources :characters
+      end
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
