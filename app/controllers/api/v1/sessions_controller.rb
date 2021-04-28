e97b6908239e2417a5fff97708b8dc3a9905e58e
Jose Figueroa <joselfigueroag@gmail.com>
2021-04-28 14:03:01 -0400

    modified controller sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,15 +6,12 @@ module Api
       def login
         @session = User.find_by(name: params[:name])
 
-        if @session 
-          render :show
-        else
-          render json: @session.errors
+        unless @session
+          render html: "Informacion errada, intentelo nuevamente"
         end
       end
 
       def show
-        @user = User.find_by(params[:id])
       end
     end
   end
