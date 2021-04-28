0de4ee8189c973b41fd644b4610971d918cc91f1
Jose Figueroa <joselfigueroag@gmail.com>
2021-04-28 14:01:40 -0400

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      # get 'users', to: 'users#index'
-      # get 'users/create', to: 'users#create'
-      resources :sessions
+      get 'sessions/login', to: 'sessions#login'
       resources :users 
       resources :movie_series do
         resources :characters
